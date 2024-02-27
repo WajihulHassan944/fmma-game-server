7170c0317ce288c839423fb3c78fd7ee6e4a1f56
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-02-27 19:17:51 +0500

    update 8

--- index.js
+++ index.js
@@ -177,10 +177,10 @@ const Category = mongoose.model('Category', categorySchema);
 
 app.post('/addCategory', async (req, res) => {
   
-  const { category } = req.body; // Destructure title and text from req.body
+  const category  = req.body.category; // Destructure title and text from req.body
   console.log(category);
   // Save the image URL, title, and text to the database
-  const newCategory = new Category({  category:category });
+  const newCategory = new Category({ category:category });
   await newCategory.save();
   res.status(200).send('Category Added Successfully');
 });
